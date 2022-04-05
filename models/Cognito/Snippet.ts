b393adb98c66fb940740041f390665f54f043e8c
Josh Marshall <josh@jmarshall.com.au>
2022-04-05 11:16:11 +1000

    Add Cognito Snippet

---
+++ models/Cognito/Snippet.ts
@@ -0,0 +1,16 @@
+import { CognitoBase } from '../Cognito/Base'
+class CognitoSnippet extends CognitoBase {
+  name: string
+  content: string
+  baseurl() {
+    return '/api/v1/cognito/snippet'
+  }
+
+  constructor(source?: Partial<CognitoSnippet>) {
+    super()
+    this.name = ''
+    this.content = ''
+    Object.assign(this, source)
+  }
+}
+export { CognitoSnippet }
