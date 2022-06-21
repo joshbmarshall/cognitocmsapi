d7644d5f612a22d86f9295c477884e439134d4d3
Josh Marshall <josh@jmarshall.com.au>
2022-06-21 12:24:53 +1000

    Add Afterpay text

---
+++ Components/AfterpayText.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-if="amount > 0">
+    Or 4 payments of ${{ amount.toFixed(2) }} with <cgn-afterpay-logo width="100" class="inline-block" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  total: {
+    type: Number,
+    required: true,
+  },
+})
+const amount = computed(() => {
+  return props.total / 4
+})
+</script>
