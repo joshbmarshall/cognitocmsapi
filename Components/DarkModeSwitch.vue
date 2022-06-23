f44b407e087beb0b27869e3708c5126a988e5878
Josh Marshall <josh@jmarshall.com.au>
2022-06-24 00:02:04 +1000

    Add dark mode switch

---
+++ Components/DarkModeSwitch.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="cursor-pointer" @click="toggleDark()">
+    <i-heroicons-solid:moon v-if="isDark" />
+    <i-heroicons-solid:sun v-else />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
