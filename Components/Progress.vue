da531c02f47bcf4a4d4e00c759b7d900bcd0ac73
Josh Marshall <josh@jmarshall.com.au>
2022-10-25 15:35:12 +1000

    Make progress colors configurable

--- Components/Progress.vue
+++ Components/Progress.vue
@@ -3,14 +3,16 @@
     <div class="flex justify-between items-center pb-1">
       <p
         tabindex="0"
-        class="focus:outline-none text-sm text-indigo-700 dark:text-indigo-600 font-bold"
+        class="focus:outline-none text-sm font-bold"
+        :class="textColor"
       >
         {{ progress }}% Complete
       </p>
     </div>
     <div class="h-1 bg-gray-300 dark:bg-slate-600 rounded">
       <div
-        class="bg-indigo-700 dark:bg-indigo-600 h-1 rounded relative"
+        class="h-1 rounded relative"
+        :class="barColor"
         :style="`width: ${progress}%`"
       />
     </div>
@@ -22,6 +24,14 @@ const props = defineProps({
   progress: {
     type: Number,
     default: 0,
+  },
+  barColor: {
+    type: String,
+    default: 'bg-indigo-700 dark:bg-indigo-600',
+  },
+  textColor: {
+    type: String,
+    default: 'text-indigo-700 dark:text-indigo-600',
   },
 })
 </script>
