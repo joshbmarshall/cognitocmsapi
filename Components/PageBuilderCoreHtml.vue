d88166054b42d7bd5ebe4f1debcd653dd0b61d3f
Josh Marshall <josh@jmarshall.com.au>
2023-01-26 17:38:15 +1000

    PageBuilder html block

---
+++ Components/PageBuilderCoreHtml.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="prose" v-html="templatevar.html" />
+</template>
+
+<script lang="ts">
+class Templatevars {
+  html?: string
+}
+</script>
+
+<script setup lang="ts">
+const props = defineProps({
+  templatevar: {
+    type: Templatevars,
+    required: true,
+  },
+})
+</script>
