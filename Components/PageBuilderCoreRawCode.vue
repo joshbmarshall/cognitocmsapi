76d06f269ad54c48c440eb8e9dfd7148c0dea9c6
Josh Marshall <josh@jmarshall.com.au>
2023-01-27 16:47:58 +1000

    add raw code

---
+++ Components/PageBuilderCoreRawCode.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="prose" v-html="templatevar.raw_html" />
+</template>
+
+<script lang="ts">
+class Templatevars {
+  raw_html?: string
+}
+</script>
+
+<script setup lang="ts">
+const props = defineProps({
+  templatevar: {
+    type: Templatevars,
+    required: true,
+  },
+})
+</script>
