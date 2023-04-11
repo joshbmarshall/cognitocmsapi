47d257422a18794903c788bca7ddbf792419dd2d
zander999 <zandertmarshall@gmail.com>
2023-04-11 13:02:58 +1000

    Added recipe fields

--- models/Fwcms/Recipe.ts
+++ models/Fwcms/Recipe.ts
@@ -3,6 +3,12 @@ import { CognitoImage } from '../Cognito/Image'
 
 class FwcmsRecipe extends CognitoBase {
   name: string
+  prep_time: number
+  cook_time: number
+  difficulty: number
+  servings: number
+  author: string
+  yield: string
   url: string
   description: string
   content: string
@@ -10,6 +16,7 @@ class FwcmsRecipe extends CognitoBase {
   method: string
   hints: string
   image: CognitoImage
+  related_recipes: Array<FwcmsRecipe>
 
   baseurl(): string {
     return '/api/v1/fwcms/recipe'
@@ -18,6 +25,12 @@ class FwcmsRecipe extends CognitoBase {
   constructor(source?: Partial<FwcmsRecipe>) {
     super()
     this.name = ''
+    this.prep_time = 0
+    this.cook_time = 0
+    this.difficulty = 0
+    this.servings = 0
+    this.author = ''
+    this.yield = ''
     this.url = ''
     this.description = ''
     this.content = ''
@@ -25,6 +38,7 @@ class FwcmsRecipe extends CognitoBase {
     this.method = ''
     this.hints = ''
     this.image = new CognitoImage()
+    this.related_recipes = []
     Object.assign(this, source)
   }
 }
