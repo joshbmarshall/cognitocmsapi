426a0ddc792d92f246b9b31f2d5e88737b3c4ec0
Josh Marshall <josh@jmarshall.com.au>
2023-06-13 15:45:34 +1000

    Add computed organisation logo output

---
+++ Components/OrganisationLogo.vue
@@ -0,0 +1,18 @@
+<template>
+  <cgn-lazy-image :image="logo" />
+</template>
+
+<script setup lang="ts">
+const lightLogo = computed(() => {
+  return usePagesStore().currentDomain.organisation_logo
+})
+const darkLogo = computed(() => {
+  return usePagesStore().currentDomain.organisation_logo_dark
+})
+const logo = computed(() => {
+  if (isDark.value && darkLogo.value) {
+    return darkLogo.value
+  }
+  return lightLogo.value
+})
+</script>
