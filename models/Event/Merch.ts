6eb5b38f1eaaef7372f6b5a19d042b79ab71041a
Josh Marshall <josh@jmarshall.com.au>
2023-06-22 13:01:22 +1000

    merch option typing

--- models/Event/Merch.ts
+++ models/Event/Merch.ts
@@ -1,11 +1,23 @@
 import { CognitoBase } from '../Cognito/Base'
 import type { CognitoPhoto } from '../Cognito/Photo'
 
+class EventMerchOption {
+  id: number
+  name: string
+  price_each: number
+  constructor() {
+    this.id = 0
+    this.name = ''
+    this.price_each = 0
+  }
+}
+
 class EventMerch extends CognitoBase {
   name: string
   description: string
   price_each: number
   photos: CognitoPhoto[]
+  options: EventMerchOption[]
 
   baseurl() {
     return '/api/v1/event/merch'
@@ -17,6 +29,7 @@ class EventMerch extends CognitoBase {
     this.description = ''
     this.price_each = 0
     this.photos = []
+    this.options = []
     Object.assign(this, source)
   }
 }
