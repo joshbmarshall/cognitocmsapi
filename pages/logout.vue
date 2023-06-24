58cb8ed350847eeb810836e26c9523a6b5ac1efe
Josh Marshall <josh@jmarshall.com.au>
2023-06-24 22:55:14 +1000

    add logout

---
+++ pages/logout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div />
+</template>
+
+<script setup lang="ts">
+import { logout } from '~cognito/plugins/axios'
+
+const router = useRouter()
+logout()
+nextTick(() => {
+  router.push('/')
+})
+</script>
