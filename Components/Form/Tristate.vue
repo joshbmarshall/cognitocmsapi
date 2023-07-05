978cb61725fa0582787a30160fbe34c9e8b5b2ee
Josh Marshall <josh@jmarshall.com.au>
2023-07-05 14:50:29 +1000

    Add yes/no dropdown

---
+++ Components/Form/Tristate.vue
@@ -0,0 +1,16 @@
+<template>
+  <cgn-form-dropdown :options="options" />
+</template>
+
+<script setup lang="ts">
+const options = [
+  {
+    name: 'No',
+    value: 0,
+  },
+  {
+    name: 'Yes',
+    value: 1,
+  },
+]
+</script>
