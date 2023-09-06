e0ffef4f3c758faae49e994195e6148220fcb66c
Josh Marshall <josh@jmarshall.com.au>
2023-09-07 09:59:40 +1000

    Fix logo download

--- getpagebuilder.js
+++ getpagebuilder.js
@@ -5,7 +5,21 @@ import { config } from '../src/config.js'
 const data = await axios.post(`${config.prodURL}/api/v1/cognito/domain/initialData`, {
   config,
 })
+
 writeFile('./src/initialData.json', JSON.stringify(data.data), (err) => {
+  if (err) {
+    throw err
+  }
+})
+
+const logo = await axios.get(`${config.baseURL}/cms/cognito/domain/favicon`, {
+  responseType: 'arraybuffer', // Important
+  headers: {
+    'Content-Type': 'application/gzip',
+  },
+})
+
+writeFile('./public/logo', logo.data, (err) => {
   if (err) {
     throw err
   }
