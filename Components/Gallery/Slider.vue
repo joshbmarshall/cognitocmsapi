1e0ed8a4c55791607e059d4be40941149cdeebc8
zander999 <zandertmarshall@gmail.com>
2023-09-12 13:03:43 +1000

    Change background to lazy image

--- Components/Gallery/Slider.vue
+++ Components/Gallery/Slider.vue
@@ -11,8 +11,8 @@
       <router-link :to="slide.link" :class="{ 'pointer-events-none': !slide.link }">
         <div class="mx-auto space-y-2 text-white lg:col-start-1 lg:row-start-1 lg:max-w-none">
           <div :class="outerClass(slide)" class="relative">
-            <div class="absolute inset-0 bg-cover bg-center" :class="imageClass(slide)" :style="{ 'background-image': `url(${slide.image.url})` }" />
-            <div class="relative flex h-[500px] flex-col items-center justify-center p-8 text-center" :class="textClass(slide)">
+            <cgn-lazy-image :class="imageClass(slide)" :image="slide.image" />
+            <div class="absolute inset-0 flex flex-col items-center justify-center p-8 text-center" :class="textClass(slide)">
               <div class="pb-2 font-title text-2xl font-semibold md:text-4xl">
                 {{ slide.heading }}
               </div>
