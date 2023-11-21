055c99b86034a0f634d531c0f5468085504b2eee
Josh Marshall <josh@jmarshall.com.au>
2023-11-21 11:27:00 +1000

    change dark modals to use darkbg

--- Components/Modal.vue
+++ Components/Modal.vue
@@ -2,12 +2,12 @@
   <div
     v-if="modelValue"
   >
-    <div class="fixed inset-0 z-[9999] h-screen w-screen bg-gray-500/75 transition-opacity dark:bg-gray-700/75" @click="close()" />
+    <div class="fixed inset-0 z-[9999] h-screen w-screen bg-gray-500/75 transition-opacity dark:bg-darkbg-700/75" @click="close()" />
     <div
-      class="fixed inset-x-0 top-0 z-[9999] mx-auto my-8 inline-block w-full max-w-4xl overflow-hidden rounded-lg bg-white shadow-xl dark:bg-gray-700"
+      class="fixed inset-x-0 top-0 z-[9999] mx-auto my-8 inline-block w-full max-w-4xl overflow-hidden rounded-lg bg-white shadow-xl dark:bg-darkbg-700"
     >
       <div
-        class="absolute right-0 top-0 h-10 w-10 select-none p-2.5 text-lg hover:bg-gray-100 dark:hover:bg-gray-600"
+        class="absolute right-0 top-0 h-10 w-10 select-none p-2.5 text-lg hover:bg-gray-100 dark:hover:bg-darkbg-600"
         @click="close()"
       >
         <i-heroicons-solid:x />
@@ -25,7 +25,7 @@
           <slot name="clean-content" />
         </div>
       </div>
-      <div class="bg-gray-50 dark:bg-gray-700 sm:flex sm:flex-row-reverse">
+      <div class="bg-gray-50 dark:bg-darkbg-700 sm:flex sm:flex-row-reverse">
         <slot name="button-footer" />
       </div>
     </div>
