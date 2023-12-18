6fd5fd5200fcc9109f4b45700afcf86a5280dfce
Josh Marshall <josh@jmarshall.com.au>
2023-12-19 00:18:05 +1000

    Do pwa refresh on client only

--- modules/router.ts
+++ modules/router.ts
@@ -1,20 +1,9 @@
 import { useRegisterSW } from 'virtual:pwa-register/vue'
-import { type UserModule } from '~/types'
+import type { UserModule } from '~/types'
 import { isLoggedIn, setRedirectAfterLogin } from '~cognito/plugins/axios'
 
 export const install: UserModule = ({ router, isClient }) => {
-  const {
-    needRefresh,
-    updateServiceWorker,
-  } = useRegisterSW()
-
   router.beforeEach((to, from, next) => {
-    if (isClient) {
-      window.scrollTo(0, 0)
-      if (needRefresh.value) {
-        updateServiceWorker()
-      }
-    }
     if (to.matched.some(record => record.meta.requiresAuth)) {
       if (!isLoggedIn()) {
         setRedirectAfterLogin(to.path)
@@ -23,5 +12,23 @@ export const install: UserModule = ({ router, isClient }) => {
       }
     }
     next()
+  })
+
+  if (!isClient) {
+    return
+  }
+
+  const {
+    needRefresh,
+    updateServiceWorker,
+  } = useRegisterSW()
+
+  router.beforeEach(() => {
+    if (isClient) {
+      window.scrollTo(0, 0)
+      if (needRefresh.value) {
+        updateServiceWorker()
+      }
+    }
   })
 }
