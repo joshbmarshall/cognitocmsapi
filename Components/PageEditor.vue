43ddae3e9905a1837045047897cb597257cc22cf
zander999 <zandertmarshall@gmail.com>
2024-07-25 11:32:17 +1000

    cleaned up page editor

--- Components/PageEditor.vue
+++ Components/PageEditor.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="size-full bg-darkbg-500 shadow-xl shadow-slate-700 dark:shadow-black">
-    <i-heroicons-solid:x class="absolute right-1 top-1 cursor-pointer" @click="emit('closeEditor')" />
-    <div class="p-2">
-      Edit page
-      <div v-for="row in pageStore.currentPage.rows" :key="row.id">
-        <div v-for="block in row.blocks" :key="block.id">
-          <div v-for="widget, idx in block.widgets" :key="idx">
-            <div v-for="templatevar, tvname in widget.templatevar" :key="tvname">
-              <cgn-widget-edit
-                v-model="widget.templatevar[tvname]" :widget-name="widget.outer" :var-name="tvname"
-                :widgets="props.widgets"
-              />
-            </div>
+  <div class="size-full overflow-y-scroll overscroll-contain bg-darkbg-500 p-2 shadow-xl shadow-slate-700 dark:shadow-black">
+    <div class="flex items-center justify-between text-lg">
+      <span>Edit {{ pageStore.currentPage.name }}</span>
+      <i-heroicons-solid:x class="cursor-pointer" @click="emit('closeEditor')" />
+    </div>
+    <div v-for="row in pageStore.currentPage.rows" :key="row.id">
+      <div v-for="block in row.blocks" :key="block.id">
+        <div v-for="widget, idx in block.widgets" :key="idx">
+          <div v-for="templatevar, tvname in widget.templatevar" :key="tvname">
+            <cgn-widget-edit
+              v-model="widget.templatevar[tvname]" :widget-name="widget.outer" :var-name="tvname"
+              :widgets="props.widgets"
+            />
           </div>
         </div>
       </div>
--- Components/PageRouter.vue
+++ Components/PageRouter.vue
@@ -1,14 +1,11 @@
 <template>
-  <router-view v-if="!showPageEditor" />
-  <div v-else class="flex">
-    <div class="w-3/4">
-      <router-view />
-    </div>
-    <div class="fixed right-0 top-0 z-50 h-screen w-1/4">
-      <cgn-page-editor :widgets="widgets" @close-editor="showPageEditor = false" />
-    </div>
+  <div :class="{ 'w-3/4': showPageEditor }">
+    <router-view />
   </div>
-  <div v-if="canShowPageEditor" class="absolute left-1 top-1 z-50 cursor-pointer">
+  <div v-if="showPageEditor" class="fixed right-0 top-0 z-50 h-screen w-1/4">
+    <cgn-page-editor :widgets="widgets" @close-editor="showPageEditor = false" />
+  </div>
+  <div v-if="canShowPageEditor" class="absolute left-1 top-1 z-50 hidden cursor-pointer sm:block">
     <i-heroicons-solid:pencil v-if="!showPageEditor" @click="showPageEditor = true" />
   </div>
   <cgn-toast-output />
