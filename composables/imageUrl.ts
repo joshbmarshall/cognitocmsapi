22b4540f25ffcf628c576433018870fb36ee0114
zander999 <zandertmarshall@gmail.com>
2024-08-05 17:07:41 +1000

    use baseurl in image url getter

--- composables/imageUrl.ts
+++ composables/imageUrl.ts
@@ -1,6 +1,8 @@
+import { config } from '~/config'
+
 export function useImageUrl() {
   const getUrl = (image_hash: string, field: string, width: number, aspect: string, extension: string = 'jpg') => {
-    return `/assets/i/${image_hash}/${field}/${aspect}/${width}.${extension}`
+    return `${config.baseURL}/assets/i/${image_hash}/${field}/${aspect}/${width}.${extension}`
   }
 
   return { getUrl }
