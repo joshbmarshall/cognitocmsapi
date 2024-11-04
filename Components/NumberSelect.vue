9a182bc317b182585b424ed38ec8890cc66baa6e
Josh Marshall <josh@jmarshall.com.au>
2024-11-04 20:44:17 +1000

    Add up/down number input

---
+++ Components/NumberSelect.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="text-center">
+    <i-heroicons-solid:chevron-up @click="inc" />
+    {{ inputValue }}
+    <i-heroicons-solid:chevron-down @click="dec" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const {
+  min,
+  max,
+} = defineProps<{
+  min: number
+  max: number
+}>()
+
+const inputValue = defineModel<any>()
+
+const inc = () => {
+  inputValue.value++
+  if (inputValue.value >= max) {
+    inputValue.value = max
+  }
+}
+const dec = () => {
+  inputValue.value--
+  if (inputValue.value <= min) {
+    inputValue.value = min
+  }
+}
+</script>
