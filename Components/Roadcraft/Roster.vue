33223db173fb65c78110d6e71b2efb9e51dc90f7
Josh Marshall <josh@jmarshall.com.au>
2025-02-05 14:59:21 +1000

    Check if educator is away AM / PM

--- Components/Roadcraft/Roster.vue
+++ Components/Roadcraft/Roster.vue
@@ -11,124 +11,126 @@
       Today
     </cgn-button>
   </div>
-  <div v-if="is_draft" class="absolute inset-0 items-center justify-center flex pointer-events-none">
-    <div class="text-9xl text-black/30 -rotate-45">
+  <div v-if="is_draft" class="pointer-events-none absolute inset-0 flex items-center justify-center">
+    <div class="-rotate-45 text-9xl text-black/30">
       DRAFT
     </div>
   </div>
   <div class="overflow-x-scroll">
     <table class="border-separate border-spacing-0 border-black">
-      <tr class="border-inherit">
-        <th class="sticky left-0 z-10 border border-inherit bg-blue-50 font-medium">
-          Date
-        </th>
-        <td v-for="day, index in days" :key="index" class="w-20 border-y border-r border-inherit text-center" :class="getDayColor(day.date)" :colspan="day.eventDays.length">
-          {{ day.date.format('d') }}
-        </td>
-      </tr>
-      <tr class="border-inherit">
-        <th class="sticky left-0 z-10 border-x border-b-2 border-inherit bg-blue-50 font-medium">
-          Day
-        </th>
-        <td v-for="day, index in days" :key="index" class="border-b-2 border-r border-inherit text-center" :class="getDayColor(day.date)" :colspan="day.eventDays.length">
-          {{ day.date.format('E') }}
-        </td>
-      </tr>
-      <tr class="border-inherit">
-        <th class="sticky left-0 z-10 h-36 border-x border-b border-inherit bg-blue-50">
-          <span class="rotate-180 text-nowrap text-xl [text-orientation:mixed] [writing-mode:vertical-rl]">Course</span>
-        </th>
-        <template v-for="day, index in days" :key="index">
-          <td v-for="eventDay in day.eventDays" :key="eventDay.event.id" class="min-w-12 border-b border-r border-inherit text-center" :class="getDayColor(day.date)">
-            <span class="rotate-180 text-nowrap leading-tight [text-orientation:mixed] [writing-mode:vertical-rl]">
-              <div :class="getStatusColor(eventDay.event.status)">
-                {{ eventDay.event.course.name }}
-                <span v-if="eventDay.am_only" class="font-bold">AM</span>
-                <span v-if="eventDay.pm_only" class="font-bold">PM</span>
-                <span v-if="eventDay.event.number_of_days > 1">D{{ eventDay.day_number }}</span>
-                <span v-if="eventDay.start_time && false"> ({{ eventDay.start_time }} - {{ eventDay.end_time }})</span>
+      <tbody>
+        <tr class="border-inherit">
+          <th class="sticky left-0 z-10 border border-inherit bg-blue-50 font-medium">
+            Date
+          </th>
+          <td v-for="day, index in days" :key="index" class="w-20 border-y border-r border-inherit text-center" :class="getDayColor(day.date)" :colspan="day.eventDays.length">
+            {{ day.date.format('d') }}
+          </td>
+        </tr>
+        <tr class="border-inherit">
+          <th class="sticky left-0 z-10 border-x border-b-2 border-inherit bg-blue-50 font-medium">
+            Day
+          </th>
+          <td v-for="day, index in days" :key="index" class="border-b-2 border-r border-inherit text-center" :class="getDayColor(day.date)" :colspan="day.eventDays.length">
+            {{ day.date.format('E') }}
+          </td>
+        </tr>
+        <tr class="border-inherit">
+          <th class="sticky left-0 z-10 h-36 border-x border-b border-inherit bg-blue-50">
+            <span class="rotate-180 text-nowrap text-xl [text-orientation:mixed] [writing-mode:vertical-rl]">Course</span>
+          </th>
+          <template v-for="day, index in days" :key="index">
+            <td v-for="eventDay in day.eventDays" :key="eventDay.event.id" class="min-w-12 border-b border-r border-inherit text-center" :class="getDayColor(day.date)">
+              <span class="rotate-180 text-nowrap leading-tight [text-orientation:mixed] [writing-mode:vertical-rl]">
+                <div :class="getStatusColor(eventDay.event.status)">
+                  {{ eventDay.event.course.name }}
+                  <span v-if="eventDay.am_only" class="font-bold">AM</span>
+                  <span v-if="eventDay.pm_only" class="font-bold">PM</span>
+                  <span v-if="eventDay.event.number_of_days > 1">D{{ eventDay.day_number }}</span>
+                  <span v-if="eventDay.start_time && false"> ({{ eventDay.start_time }} - {{ eventDay.end_time }})</span>
+                </div>
+                <div>
+                  {{ eventDay.event.customer?.name }}
+                </div>
+              </span>
+            </td>
+            <td v-if="day.eventDays.length < 1" class="min-w-10 border-b border-r border-inherit text-center" :class="getDayColor(day.date)" />
+          </template>
+        </tr>
+        <tr class="border-inherit">
+          <th class="sticky left-0 z-10 border-x border-b-2 border-inherit bg-blue-50">
+            <span class="text-nowrap px-1 text-xs">Student Numbers</span>
+          </th>
+          <template v-for="day, index in days" :key="index">
+            <td v-for="eventDay in day.eventDays" :key="eventDay.event.id" class="text-nowrap border-b-2 border-r border-inherit px-0.5 text-center text-xs" :class="getDayColor(day.date)">
+              {{ eventDay.event.student_numbers }}
+            </td>
+            <td v-if="day.eventDays.length < 1" class="min-w-10 border-b-2 border-r border-inherit text-center" :class="getDayColor(day.date)" />
+          </template>
+        </tr>
+        <tr class="border-inherit">
+          <th class="sticky left-0 z-10 border-x border-b-2 border-inherit bg-blue-50">
+            <span class="text-nowrap px-1 text-xs">Vehicle Numbers</span>
+          </th>
+          <template v-for="day, index in days" :key="index">
+            <td v-for="eventDay in day.eventDays" :key="eventDay.event.id" class="text-nowrap border-b-2 border-r border-inherit px-0.5 text-center text-xs" :class="getDayColor(day.date)">
+              {{ eventDay.event.vehicle_numbers }}
+            </td>
+            <td v-if="day.eventDays.length < 1" class="min-w-10 border-b-2 border-r border-inherit text-center" :class="getDayColor(day.date)" />
+          </template>
+        </tr>
+        <tr v-for="educator in educators" :key="educator.id" class="text-nowrap border-inherit text-center text-sm odd:bg-[#ddd9c3] even:bg-white">
+          <th class="sticky left-0 z-10 border-x border-b border-inherit bg-inherit px-0.5">
+            {{ educator.first_name }} {{ educator.last_name }}
+          </th>
+          <td v-for="day, index in days" :key="index" class="m-0 max-w-24 border-b border-r border-inherit" :colspan="day.eventDays.length" :class="{ 'bg-neutral-500 text-white': educatorIsAway(day, educator) }">
+            <div v-for="(status, sidx) in getEducatorDayStatus(day, educator)" :key="sidx" class="px-0.5">
+              <div :class="status.class">
+                {{ status.text }}
               </div>
-              <div>
-                {{ eventDay.event.customer?.name }}
+            </div>
+          </td>
+        </tr>
+        <tr v-for="facility in facilities" :key="facility.id" class="text-nowrap border-inherit text-center text-sm">
+          <th class="sticky left-0 z-10 border-x border-t border-inherit bg-blue-50">
+            {{ facility.name }}
+          </th>
+          <td v-for="day, index in days" :key="index" class="m-0 max-w-24 border-r border-t border-inherit" :colspan="day.eventDays.length" :class="getDayColor(day.date)">
+            <div v-for="status, idx in getFacilityDayStatus(day, facility)" :key="idx" class="truncate px-0.5" :title="status.text">
+              <div :class="status.class">
+                {{ status.text }}
               </div>
-            </span>
-          </td>
-          <td v-if="day.eventDays.length < 1" class="min-w-10 border-b border-r border-inherit text-center" :class="getDayColor(day.date)" />
-        </template>
-      </tr>
-      <tr class="border-inherit">
-        <th class="sticky left-0 z-10 border-x border-b-2 border-inherit bg-blue-50">
-          <span class="text-nowrap px-1 text-xs">Student Numbers</span>
-        </th>
-        <template v-for="day, index in days" :key="index">
-          <td v-for="eventDay in day.eventDays" :key="eventDay.event.id" class="text-nowrap border-b-2 border-r border-inherit px-0.5 text-center text-xs" :class="getDayColor(day.date)">
-            {{ eventDay.event.student_numbers }}
-          </td>
-          <td v-if="day.eventDays.length < 1" class="min-w-10 border-b-2 border-r border-inherit text-center" :class="getDayColor(day.date)" />
-        </template>
-      </tr>
-      <tr class="border-inherit">
-        <th class="sticky left-0 z-10 border-x border-b-2 border-inherit bg-blue-50">
-          <span class="text-nowrap px-1 text-xs">Vehicle Numbers</span>
-        </th>
-        <template v-for="day, index in days" :key="index">
-          <td v-for="eventDay in day.eventDays" :key="eventDay.event.id" class="text-nowrap border-b-2 border-r border-inherit px-0.5 text-center text-xs" :class="getDayColor(day.date)">
-            {{ eventDay.event.vehicle_numbers }}
-          </td>
-          <td v-if="day.eventDays.length < 1" class="min-w-10 border-b-2 border-r border-inherit text-center" :class="getDayColor(day.date)" />
-        </template>
-      </tr>
-      <tr v-for="educator in educators" :key="educator.id" class="text-nowrap border-inherit text-center text-sm odd:bg-[#ddd9c3] even:bg-white">
-        <th class="sticky left-0 z-10 border-x border-b border-inherit bg-inherit px-0.5">
-          {{ educator.first_name }} {{ educator.last_name }}
-        </th>
-        <td v-for="day, index in days" :key="index" class="m-0 max-w-24 border-b border-r border-inherit" :colspan="day.eventDays.length" :class="{ 'bg-neutral-500 text-white': educatorIsAway(day, educator) }">
-          <div v-for="(status, sidx) in getEducatorDayStatus(day, educator)" :key="sidx" class="px-0.5">
-            <div :class="status.class">
-              {{ status.text }}
             </div>
-          </div>
-        </td>
-      </tr>
-      <tr v-for="facility in facilities" :key="facility.id" class="text-nowrap border-inherit text-center text-sm">
-        <th class="sticky left-0 z-10 border-x border-t border-inherit bg-blue-50">
-          {{ facility.name }}
-        </th>
-        <td v-for="day, index in days" :key="index" class="m-0 max-w-24 border-r border-t border-inherit" :colspan="day.eventDays.length" :class="getDayColor(day.date)">
-          <div v-for="status, idx in getFacilityDayStatus(day, facility)" :key="idx" class="truncate px-0.5" :title="status.text">
-            <div :class="status.class">
-              {{ status.text }}
+          </td>
+        </tr>
+        <tr class="border-inherit">
+          <th class="sticky left-0 z-10 h-10 border-x border-t-2 border-inherit bg-blue-50">
+            Accommodation
+          </th>
+          <td v-for="day, index in days" :key="index" class="border-r border-t-2 border-inherit text-center text-sm" :class="getDayColor(day.date)" :colspan="day.eventDays.length">
+            <div v-for="accommodation in day.requiredAccommodation" :key="accommodation.accommodation.id">
+              {{ accommodation.accommodation.name }}
+              ({{ accommodation.residents.join(', ') }})
             </div>
-          </div>
-        </td>
-      </tr>
-      <tr class="border-inherit">
-        <th class="sticky left-0 z-10 h-10 border-x border-t-2 border-inherit bg-blue-50">
-          Accommodation
-        </th>
-        <td v-for="day, index in days" :key="index" class="border-r border-t-2 border-inherit text-center text-sm" :class="getDayColor(day.date)" :colspan="day.eventDays.length">
-          <div v-for="accommodation in day.requiredAccommodation" :key="accommodation.accommodation.id">
-            {{ accommodation.accommodation.name }}
-            ({{ accommodation.residents.join(', ') }})
-          </div>
-        </td>
-      </tr>
-      <tr class="border-inherit">
-        <th class="sticky left-0 z-10 border-x border-t-2 border-inherit bg-blue-50 font-medium">
-          Date
-        </th>
-        <td v-for="day, index in days" :key="index" class="border-r border-t-2 border-inherit text-center" :class="getDayColor(day.date)" :colspan="day.eventDays.length">
-          {{ day.date.format('d') }}
-        </td>
-      </tr>
-      <tr class="border-inherit">
-        <th class="sticky left-0 z-10 border border-inherit bg-blue-50 font-medium">
-          Day
-        </th>
-        <td v-for="day, index in days" :key="index" class="border-y border-r border-inherit text-center" :class="getDayColor(day.date)" :colspan="day.eventDays.length">
-          {{ day.date.format('E') }}
-        </td>
-      </tr>
+          </td>
+        </tr>
+        <tr class="border-inherit">
+          <th class="sticky left-0 z-10 border-x border-t-2 border-inherit bg-blue-50 font-medium">
+            Date
+          </th>
+          <td v-for="day, index in days" :key="index" class="border-r border-t-2 border-inherit text-center" :class="getDayColor(day.date)" :colspan="day.eventDays.length">
+            {{ day.date.format('d') }}
+          </td>
+        </tr>
+        <tr class="border-inherit">
+          <th class="sticky left-0 z-10 border border-inherit bg-blue-50 font-medium">
+            Day
+          </th>
+          <td v-for="day, index in days" :key="index" class="border-y border-r border-inherit text-center" :class="getDayColor(day.date)" :colspan="day.eventDays.length">
+            {{ day.date.format('E') }}
+          </td>
+        </tr>
+      </tbody>
     </table>
   </div>
 </template>
@@ -182,6 +184,8 @@ class RoadcraftPlannerDay {
 
   staffUnavailable: {
     staff_id: number
+    from: string
+    to: string
   }[]
 
   staffTravelling: {
@@ -275,6 +279,18 @@ const getStatusColor = (status: string) => {
 const getEducatorDayStatus = (day: RoadcraftPlannerDay, educator: RoadcraftPlannerEducator): RoadcraftCellColouredText[] => {
   const unavailable = day.staffUnavailable.find(e => e.staff_id == educator.id)
   if (unavailable) {
+    // Check is if AM
+    const noon = day.date.addMinutes(12 * 60)
+    const unavailable_to = new CognitoTime(unavailable.to)
+    const unavailable_from = new CognitoTime(unavailable.from)
+    if (unavailable_to.isBefore(noon.addMinutes(1))) {
+      return [new RoadcraftCellColouredText({ text: 'AWAY AM' })]
+    }
+    // Check is if PM
+    if (unavailable_from.isAfter(noon.subMinutes(1))) {
+      return [new RoadcraftCellColouredText({ text: 'AWAY PM' })]
+    }
+
     return [new RoadcraftCellColouredText({ text: 'AWAY' })]
   }
   const status = []
@@ -378,6 +394,8 @@ const getPlannerData = () => {
         }
         staffUnavailable {
           staff_id
+          from
+          to
         }
         staffTravelling {
           staff_id
