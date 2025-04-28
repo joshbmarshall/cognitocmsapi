7cca062b69ad3a79e90fc64a01667ac260c40003
zander999 <zandertmarshall@gmail.com>
2025-04-28 12:44:22 +1000

    video to use youtube video if it exists

--- Components/PageBuilder/CoreVideo.vue
+++ Components/PageBuilder/CoreVideo.vue
@@ -1,8 +1,15 @@
 <template>
+  <iframe
+    v-if="video?.youtube_id"
+    class="aspect-video w-full"
+    :src="`https://www.youtube.com/embed/${video.youtube_id}`"
+  />
   <video
+    v-else
     controls
     playsinline
-    :src="videoUrl.getUrl(props.templatevar.video)"
+    :src="videoUrl.getUrl(props.templatevar.video || '')"
+    class="w-full"
   >
     <source
       type="video/mp4"
@@ -11,6 +18,9 @@
 </template>
 
 <script lang="ts">
+import { gql } from 'graphql-request'
+import { $axios } from '~cognito/plugins/axios'
+
 class Templatevars {
   video?: string
 }
@@ -24,4 +34,18 @@ const props = defineProps({
   },
 })
 const videoUrl = useVideoUrl()
+
+const video = ref<{ youtube_id?: string }>()
+const loadVideo = async () => {
+  const data = await $axios.graphql(gql`query ($id: ID) {
+      cognitoVideo(id: $id) {
+        youtube_id
+      }
+    }`, {
+    id: props.templatevar.video,
+  })
+  return (data.cognitoVideo)
+}
+onMounted(async () => video.value = await loadVideo())
+watch(() => props.templatevar.video, async () => video.value = await loadVideo())
 </script>
