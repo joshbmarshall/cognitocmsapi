dc6c263a1024743ed8612d3276d748f63e3b7cf1
Josh Marshall <josh@jmarshall.com.au>
2025-09-30 12:16:07 +1000

    Set minimum afterpay amount to show

--- Components/Afterpay/Text.vue
+++ Components/Afterpay/Text.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="amount > 99">
+  <div v-if="props.total >= props.minimumAmount">
     Or 4 payments of ${{ amount.toFixed(2) }} with <cgn-afterpay-logo width="100" class="inline-block" />
   </div>
 </template>
@@ -9,6 +9,11 @@ const props = defineProps({
   total: {
     type: Number,
     required: true,
+  },
+  minimumAmount: {
+    type: Number,
+    required: false,
+    default: 0,
   },
 })
 const amount = computed(() => {
