eaebc85f60839833728c247fc83e571197e1b67f
Josh Marshall <josh@jmarshall.com.au>
2025-10-30 13:58:34 +1000

    feat: write tsconfig.json on build with gql tada

--- getpagebuilder.js
+++ getpagebuilder.js
@@ -32,3 +32,54 @@ writeFile('.graphqlrc', JSON.stringify({
     throw err
   }
 })
+
+writeFile('tsconfig.json', `{
+  "compilerOptions": {
+    "target": "ESNext",
+    "jsx": "preserve",
+    "lib": ["DOM", "ESNext"],
+    "baseUrl": ".",
+    "module": "ESNext",
+    "moduleResolution": "node",
+    "paths": {
+      "~/*": ["src/*"],
+      "~cognito/*": ["cognitocmsapi/*"]
+    },
+    "resolveJsonModule": true,
+    "types": [
+      "vitest",
+      "vite/client",
+      "vue/ref-macros",
+      "vite-plugin-pages/client",
+      "vite-plugin-vue-layouts/client",
+      "vite-plugin-pwa/client",
+      "unplugin-vue-macros/macros-global"
+    ],
+    "allowJs": true,
+    "strict": true,
+    "strictNullChecks": true,
+    "noUnusedLocals": true,
+    "esModuleInterop": true,
+    "forceConsistentCasingInFileNames": true,
+    "isolatedModules": true,
+    "skipLibCheck": true,
+    "plugins": [
+      {
+        "name": "gql.tada/ts-plugin",
+        "schema": "${config.baseURL}/graphql",
+        "tadaOutputLocation": "src/graphql-env.d.ts"
+      }
+    ]
+  },
+  "vueCompilerOptions": {
+    "plugins": [
+      "@vue-macros/volar/define-models",
+      "@vue-macros/volar/define-slots"
+    ]
+  },
+  "exclude": ["dist", "node_modules", "cypress"]
+}\n`, (err) => {
+  if (err) {
+    throw err
+  }
+})
