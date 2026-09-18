a24ae12e556ded5f02002efdf387db9412bf22fc
Josh Marshall <josh@jmarshall.com.au>
2026-09-18 13:04:10 +1000

    feat: modal teleports to body for z-index fixes

--- Components/Modal.vue
+++ Components/Modal.vue
@@ -1,13 +1,14 @@
 <template>
-  <div
+  <teleport
+  to="body"
     v-if="modelValue"
   >
     <div class="fixed inset-0 z-[9999] h-screen w-screen bg-gray-500/75 transition-opacity dark:bg-black/60" @click="close()" />
     <div
-      class="fixed inset-x-0 top-0 z-[9999] mx-auto my-8 inline-block w-full max-w-4xl overflow-hidden rounded-lg bg-white shadow-xl dark:bg-darkbg-700"
+      class="dark:bg-darkbg-700 fixed inset-x-0 top-0 z-[9999] mx-auto my-8 inline-block w-full max-w-4xl overflow-hidden rounded-lg bg-white shadow-xl"
     >
       <div
-        class="absolute right-0 top-0 h-10 w-10 select-none p-2.5 text-lg hover:bg-gray-100 dark:hover:bg-darkbg-600"
+        class="dark:hover:bg-darkbg-600 absolute right-0 top-0 size-10 select-none p-2.5 text-lg hover:bg-gray-100"
         @click="close()"
       >
         <i-heroicons-solid:x />
@@ -25,11 +26,11 @@
           <slot name="clean-content" />
         </div>
       </div>
-      <div class="bg-gray-50 dark:bg-darkbg-700 sm:flex sm:flex-row-reverse">
+      <div class="dark:bg-darkbg-700 bg-gray-50 sm:flex sm:flex-row-reverse">
         <slot name="button-footer" />
       </div>
     </div>
-  </div>
+  </teleport>
 </template>
 
 <script setup lang="ts">
