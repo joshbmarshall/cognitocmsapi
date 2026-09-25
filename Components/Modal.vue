8b0af43631a450fd949ed9b4e0159e1161b7d99b
Josh Marshall <josh@jmarshall.com.au>
2026-09-25 12:24:46 +1000

    fix: iOS bug made the modal 30px high

--- Components/Modal.vue
+++ Components/Modal.vue
@@ -3,30 +3,32 @@
     v-if="modelValue"
   >
     <div class="fixed inset-0 z-[9999] h-screen w-screen bg-gray-500/75 transition-opacity dark:bg-black/60" @click="close()" />
-    <div
-      class="dark:bg-darkbg-700 fixed inset-0 z-[9999] m-auto flex h-fit max-h-[85dvh] w-full max-w-4xl flex-col overflow-hidden rounded-lg bg-white shadow-xl"
-    >
+    <div class="pointer-events-none fixed inset-0 z-[9999] flex items-center justify-center p-4 sm:p-6">
       <div
-        class="dark:hover:bg-darkbg-600 absolute right-0 top-0 size-10 select-none p-2.5 text-lg hover:bg-gray-100"
-        @click="close()"
+        class="dark:bg-darkbg-700 pointer-events-auto relative flex max-h-[85dvh] w-full max-w-4xl flex-col overflow-hidden rounded-lg bg-white shadow-xl"
       >
-        <i-heroicons-solid:x />
-      </div>
-      <div class="min-h-0 flex-1 overflow-y-scroll overscroll-contain break-words px-4 py-5 sm:p-6" :class="{ 'max-h-[500px]': !props.fullheight }">
-        <div class="flex items-start">
-          <div>
-            <slot name="icon" />
+        <div
+          class="dark:hover:bg-darkbg-600 absolute right-0 top-0 size-10 select-none p-2.5 text-lg hover:bg-gray-100"
+          @click="close()"
+        >
+          <i-heroicons-solid:x />
+        </div>
+        <div class="min-h-0 flex-1 overflow-y-scroll overscroll-contain break-words px-4 py-5 sm:p-6" :class="{ 'max-h-[500px]': !props.fullheight }">
+          <div class="flex items-start">
+            <div>
+              <slot name="icon" />
+            </div>
+            <div class="ml-4 min-w-0 text-left">
+              <slot name="content" />
+            </div>
           </div>
-          <div class="ml-4 min-w-0 text-left">
-            <slot name="content" />
+          <div class="text-left">
+            <slot name="clean-content" />
           </div>
         </div>
-        <div class="text-left">
-          <slot name="clean-content" />
+        <div class="dark:bg-darkbg-700 shrink-0 bg-gray-50 sm:flex sm:flex-row-reverse">
+          <slot name="button-footer" />
         </div>
-      </div>
-      <div class="dark:bg-darkbg-700 shrink-0 bg-gray-50 sm:flex sm:flex-row-reverse">
-        <slot name="button-footer" />
       </div>
     </div>
   </div>
